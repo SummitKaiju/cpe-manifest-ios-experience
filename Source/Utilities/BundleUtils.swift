e35fbd4f8ec3ae2ab915375fb8c2f13a351bec34
Imran Saadi <imran.saadi@warnerbros.com>
2018-08-14 02:16:04 -0700

    - Updated library to utilize Dynamic Frameworks for projects not using a dependency management system such as CocoaPods. Also added initial integrate for Carthage.

--- Source/Utilities/BundleUtils.swift
+++ Source/Utilities/BundleUtils.swift
@@ -7,10 +7,21 @@ import Foundation
 extension Bundle {
 
     static var frameworkResources: Bundle {
+        
+        // CocoaPod using bundles
         if let resourcesBundleURL = Bundle(for: HomeViewController.self).url(forResource: "CPEExperience", withExtension: "bundle"), let resourcesBundle = Bundle(url: resourcesBundleURL) {
             return resourcesBundle
         }
+        
+        // Init resourcesBundle
+        let resourcesBundle = Bundle(for: HomeViewController.self)
+        
+        // Carthage / Dynamic Framework
+        if !resourcesBundle.bundlePath.isEmpty && resourcesBundle.bundleURL.pathExtension == "framework" {
+            return resourcesBundle
+        }
 
+        // All Else
         return Bundle.main
     }
 
